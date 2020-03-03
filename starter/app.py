8cec8dfd3780d5ae6bb68b309179e5616e1ed101
Vasili Anoshin <vasilianoshin@alpha-beta.co.il>
2020-03-03 18:21:20 +0200

    add changes to app.py

--- starter/app.py
+++ starter/app.py
@@ -3,6 +3,19 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+
+#  Movies
+#  ----------------------------------------------------------------
+@app.route('/movies')
+def movies():
+
+
+#  Actors
+#  ----------------------------------------------------------------
+@app.route('/actors')
+def actors():
+
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
