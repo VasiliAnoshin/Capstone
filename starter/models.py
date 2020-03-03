8d5d96a4b7a3e2c0973e1f8f93218804c9fe1c7c
Vasili Anoshin <vasilianoshin@alpha-beta.co.il>
2020-03-03 15:32:10 +0200

    Db models

--- starter/models.py
+++ starter/models.py
@@ -1,1 +1,14 @@
 
+class Movie(db.Model):
+     __tablename__ = 'Movie'
+
+     id = db.Column(db.Integer, primary_key=True)
+     title =  db.Column(db.String)
+     release_date = db.Column(db.DateTime())
+
+class Actors(db.Model):
+    __tablename__ = 'Actors'
+
+    name = db.Column(db.String)
+    gender = db.Column(db.String(10))
+    age = db.Column(db.Integer)
