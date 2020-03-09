a4e702d581e7486b0ff396c0dcbcd400b04d3863
Vasili Anoshin <vasilianoshin@alpha-beta.co.il>
2020-03-09 18:50:19 +0200

    start working on test

--- starter/app.py
+++ starter/app.py
@@ -11,172 +11,178 @@ import sys
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
-app = create_app()
-migrate = Migrate(app,db)
-
 def data_format(data):
   return [d.format() for d in data]
 
-#  Movies
-#  ----------------------------------------------------------------
-@app.route('/movies')
-@requires_auth('get:movies')
-def get_movies(payload):
-  query = Movie.query.all()
+def create_app(test_config=None):
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+  
+  #  Movies
+  #  ----------------------------------------------------------------
+  @app.route('/movies')
+  @requires_auth('get:movies')
+  def get_movies(payload):
+    query = Movie.query.all()
 
-  return jsonify({
-            'movies': data_format(query),
-            'success': True
-  })
-
-@app.route('/movies/create', methods=['POST'])
-@requires_auth('post:movies')
-def create_movie(payload):
-  try:
-    req=request.get_json()
-    movie = Movie(title=req['title'], release_date=req['release_date'])
-    db.session.add(movie)
-    db.session.commit()
     return jsonify({
-          "success":True
-      })
-  except:
-    print(sys.exc_info())
-    db.session.rollback()
-    abort(404)
+              'movies': data_format(query),
+              'success': True
+    })
 
-@app.route('/movies/<int:id>', methods=['DELETE'])
-@requires_auth('delete:movie')
-def delete_movie(payload,id):
+  @app.route('/movies/create', methods=['POST'])
+  @requires_auth('post:movies')
+  def create_movie(payload):
     try:
+      req=request.get_json()
+      movie = Movie(title=req['title'], release_date=req['release_date'])
+      db.session.add(movie)
+      db.session.commit()
+      return jsonify({
+            "success":True
+        })
+    except:
+      print(sys.exc_info())
+      db.session.rollback()
+      abort(404)
+
+  @app.route('/movies/<int:id>', methods=['DELETE'])
+  @requires_auth('delete:movie')
+  def delete_movie(payload,id):
+      try:
+        movie = Movie.query.filter(Movie.id==id).one_or_none()
+        db.session.delete(movie)
+        db.session.commit()
+        return jsonify({
+            'success':True
+        })
+      except:
+        abort(404)
+
+  @app.route('/movies/<int:id>', methods=['PATCH'])
+  @requires_auth('patch:movie')
+  def update_movie(payload,id):
+    try:
+      req = request.get_json()
       movie = Movie.query.filter(Movie.id==id).one_or_none()
-      db.session.delete(movie)
+      if('title' in req):
+        movie.title = req['title']
+      if('release_date' in req):
+        movie.release_date = req['release_date']
+        
       db.session.commit()
       return jsonify({
           'success':True
-      })
+        })
     except:
       abort(404)
 
-@app.route('/movies/<int:id>', methods=['PATCH'])
-@requires_auth('patch:movie')
-def update_movie(payload,id):
-  try:
-    req = request.get_json()
-    movie = Movie.query.filter(Movie.id==id).one_or_none()
-    if('title' in req):
-      movie.title = req['title']
-    if('release_date' in req):
-      movie.release_date = req['release_date']
+  #  Actors
+  #  ----------------------------------------------------------------
+  @app.route('/actors')
+  @requires_auth('get:actors')
+  def get_actors(payload):
+    query=Actors.query.all()
       
-    db.session.commit()
     return jsonify({
-        'success':True
-      })
-  except:
-    abort(404)
+              'actors': data_format(query),
+              'success': True
+    })
 
-#  Actors
-#  ----------------------------------------------------------------
-@app.route('/actors')
-@requires_auth('get:actors')
-def get_actors(payload):
-  query=Actors.query.all()
-    
-  return jsonify({
-            'actors': data_format(query),
-            'success': True
-  })
-
-@app.route('/actors/create', methods=['POST'])
-@requires_auth('post:actors')
-def create_actors(payload):
-  try:
-    req=request.get_json()
-    actor = Actors(name=req['name'], gender=req['gender'],age=req['age'])
-    db.session.add(actor)
-    db.session.commit()
-    return jsonify({
-          "success":True
-      })
-  except:
-    db.session.rollback()
-    abort(404)
-
-@app.route('/actors/<int:id>', methods=['DELETE'])
-@requires_auth('delete:actor')
-def delete_actor(payload,id):
+  @app.route('/actors/create', methods=['POST'])
+  @requires_auth('post:actors')
+  def create_actors(payload):
     try:
+      req=request.get_json()
+      actor = Actors(name=req['name'], gender=req['gender'],age=req['age'])
+      db.session.add(actor)
+      db.session.commit()
+      return jsonify({
+            "success":True
+        })
+    except:
+      db.session.rollback()
+      abort(404)
+
+  @app.route('/actors/<int:id>', methods=['DELETE'])
+  @requires_auth('delete:actor')
+  def delete_actor(payload,id):
+      try:
+        actor = Actors.query.filter(Actors.id==id).one_or_none()
+        db.session.delete(actor)
+        db.session.commit()
+        return jsonify({
+            'success':True
+        })
+      except:
+        abort(404)
+
+  @app.route('/actors/<int:id>', methods=['PATCH'])
+  @requires_auth('patch:actor')
+  def update_actor(payload,id):
+    try:
+      req = request.get_json()
       actor = Actors.query.filter(Actors.id==id).one_or_none()
-      db.session.delete(actor)
+      if('name' in req):
+        actor.name = req['name']
+      if('gender' in req):
+        actor.gender = req['gender']
+      if('age' in req):
+        actor.age = req['age']
+        
       db.session.commit()
       return jsonify({
           'success':True
-      })
+        })
     except:
       abort(404)
 
-@app.route('/actors/<int:id>', methods=['PATCH'])
-@requires_auth('patch:actor')
-def update_actor(payload,id):
-  try:
-    req = request.get_json()
-    actor = Actors.query.filter(Actors.id==id).one_or_none()
-    if('name' in req):
-      actor.name = req['name']
-    if('gender' in req):
-      actor.gender = req['gender']
-    if('age' in req):
-      actor.age = req['age']
-      
-    db.session.commit()
-    return jsonify({
-        'success':True
-      })
-  except:
-    abort(404)
+  #  Error Wrappers
+  #  ----------------------------------------------------------------
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+        "success": False,
+        "error": 404,
+        "message":"resourse not found"
+    }), 404
 
-#  Error Wrappers
-#  ----------------------------------------------------------------
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({
-      "success": False,
-      "error": 404,
-      "message":"resourse not found"
-  }), 404
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+        "success": False,
+        "error": 422,
+        "message":"unprocessable"
+    }), 422
 
-@app.errorhandler(422)
-def unprocessable(error):
-    return jsonify({
-      "success": False,
-      "error": 422,
-      "message":"unprocessable"
-  }), 422
+  @app.errorhandler(405)
+  def method_not_allowed(error):
+      return jsonify({
+        'code': 405,
+        'success': False,
+        'message': 'method not allowed'
+    }), 405
 
-@app.errorhandler(405)
-def method_not_allowed(error):
-    return jsonify({
-      'code': 405,
-      'success': False,
-      'message': 'method not allowed'
-  }), 405
+  @app.errorhandler(500)
+  def internal_server_error(error):
+      return jsonify({
+        'code': 500,
+        'success': False,
+        'message': 'server error'
+      }), 500  
 
-@app.errorhandler(500)
-def internal_server_error(error):
-    return jsonify({
-      'code': 500,
-      'success': False,
-      'message': 'server error'
-    }), 500  
+  @app.errorhandler(AuthError)
+  def internal_auth_error(error):
+      return jsonify({
+        'error': error.error,
+        'success': False,
+        'message': error.status_code
+      }), error.status_code
 
-@app.errorhandler(AuthError)
-def internal_auth_error(error):
-    return jsonify({
-      'error': error.error,
-      'success': False,
-      'message': error.status_code
-    }), error.status_code
+  return app
+
+app = create_app()
 
 if __name__ == '__main__':
   #app.run()
--- starter/test_app.py
+++ starter/test_app.py
@@ -3,35 +3,32 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 from app import create_app
-from models import Movie,Actors
-# from init import create_app
+from models import Movie,Actors,setup_db
+import json
 
 class CapstoneTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
-        # self.client = self.app.test_client()
+        self.client = self.app.test_client()
         self.database_name = "Capstone"
         self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
-
-        # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
-            # create all tables
             self.db.create_all()
         
         def tearDown(self):
             """Executed after each test"""
             pass
     
-    def test_get_actors_without_authorization_token(self):
+    def test_get_actors_without_token(self):
         res = self.client.get('/actors')
         self.assertEqual(res.status_code, 401)
     
-    def test_get_movies_without_authorization_token(self):
+    def test_get_movies_without_token(self):
         res = self.client.get('/actors')
         self.assertEqual(res.status_code, 401)
         
