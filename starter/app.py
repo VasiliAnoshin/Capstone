61048dbc3362e3ea26edcbce2f349d413d2464a1
Vasili Anoshin <vasilianoshin@alpha-beta.co.il>
2020-03-10 13:05:59 +0200

    add several tests

--- starter/app.py
+++ starter/app.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   CORS(app)
-  
+
   #  Movies
   #  ----------------------------------------------------------------
   @app.route('/movies')
@@ -185,5 +185,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-  #app.run()
   app.run(host='0.0.0.0', port=8080, debug=True)
--- starter/test_app.py
+++ starter/test_app.py
@@ -6,6 +6,8 @@ from app import create_app
 from models import Movie,Actors,setup_db
 import json
 
+Casting_Director_JWT = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5VUXdRa0l3UkVFM1JEWXlNalZGUVVRNFFrUkJOa1UwTXpVNE56azJOekJGTlVORE5ERTFSZyJ9.eyJpc3MiOiJodHRwczovL2ZzYmVuZnJhbmtsaW4uYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA3OTQ1Mjg2NDA2MTkzOTU3MTE1IiwiYXVkIjpbIkNhcHN0b25lIiwiaHR0cHM6Ly9mc2JlbmZyYW5rbGluLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1ODM4MjQ4ODQsImV4cCI6MTU4MzkxMTI4NCwiYXpwIjoibExhcEM1TFE1VW02ZnlHeExTOHF3RE9Cd1FSTXJnM1ciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9yIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvciIsInBhdGNoOm1vdmllIiwicG9zdDphY3RvcnMiXX0.Fx4K6AqHR9ZyphVd5zcWzLH3khCAjT8Peiqg4T0LumJftqVo0F2ZCyQDQhVBIOKrRsklOpU0KtQ0blBE4GQ2HU_L0ifqvvbsT2TS7lcqmmgi-IfUz1-wHseRit_2mz3X7H_2Rm6bmYrHEgYYK3DSVqZVMMpCR7_AdF2X7zPyivukDe1lRmpc4Ka2SWRrJJanvVWCjrI3D-lJWfcyoMxwJ55rA-K10puTZJJdJCliicOoaGbbRJcCt9k6SnNdvyXXACYPmFXxiLRvErOJCmGFylUq7VAK5kYBaYDjLtYWjTCJLJB4Eou-XNnkE2Gt8NUdoKFafLram4w2rilEK0mAsA"
+
 class CapstoneTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
@@ -31,6 +33,40 @@ class CapstoneTestCase(unittest.TestCase):
     def test_get_movies_without_token(self):
         res = self.client.get('/actors')
         self.assertEqual(res.status_code, 401)
+    
+    def test_get_actors(self):
+        res = self.client.get(
+            '/actors',
+            headers={
+                "Authorization": f"Bearer {Casting_Director_JWT}"
+            }
+        )
+        self.assertEqual(res.status_code, 200)
+    
+    def test_get_movies(self):
+        res = self.client.get(
+            '/movies',
+            headers={
+                "Authorization": f"Bearer {Casting_Director_JWT}"
+            }
+        )
+        self.assertEqual(res.status_code, 200)
+    
+    def test_create_movies_without_permissions(self):
+        res = self.client.post(
+            '/movies/create',
+            json={
+                "title": "James Bond",
+                "release_date": "2020-01-01"
+            },
+            headers={
+                "Authorization": f"Bearer {Casting_Director_JWT}"
+            }
+        )
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+
+    
         
 # Make the tests conveniently executable
 if __name__ == "__main__":
